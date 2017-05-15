5a7ee30daea090aaa5b1025a7bc8d0e59fd2d388
Fernando Paschualetto <paschualetto@gmail.com>
2017-05-15 19:54:33 -0300

    Entidade Cidade

--- src/main/java/com/digitalbank/model/business/Cidade.java
+++ src/main/java/com/digitalbank/model/business/Cidade.java
@@ -15,7 +15,32 @@ public class Cidade extends AbstractEntity {
 
     @Column(length = 255, nullable = false, unique = true)
     private String nome;
-    
+
     @ManyToOne
     private Estado estado;
+
+    public Cidade() {
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
+
+    @Override
+    public String toString() {
+        return "Cidade{" + "nome=" + nome + ", estado=" + estado + '}';
+    }
+
 }
